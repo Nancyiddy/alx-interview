21a507e8cf47bf066f143e4dc50dd8547af92125
Nancy Idiong <80394179+Nancyiddy@users.noreply.github.com>
2023-07-24 17:54:31 +0100

    0-generator.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,47 @@
-!/usr/bin/python3
-"""
-Log parsing
-"""
-import sys
+#!/usr/bin/python3
+""" script that reads stdin line by line and computes metrics """
 
+if __name__ == '__main__':
 
-def print_metrics(file_size, status_codes):
-    """
-    Print metrics
-    """
-    print("File size: {}".format(file_size))
-    codes_sorted = sorted(status_codes.keys())
-    for code in codes_sorted:
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
+    import sys
 
+    def print_results(statusCodes, fileSize):
+        """ Print statistics """
+        print("File size: {:d}".format(fileSize))
+        for statusCode, times in sorted(statusCodes.items()):
+            if times:
+                print("{:s}: {:d}".format(statusCode, times))
 
-codes_count = {'200': 2, '401': 1, '403': 1, '404': 1, '405': 1, '500': 3,
-               '200': 3, '301': 2, '400': 1, '401': 2, '403': 3, '404': 4, '405': 2, '500': 3,
-             '200': 3, '301': 3, '400': 4, '401': 2, '403': 5, '404': 5, '405': 4, '500': 4,
-              '200': 4, '301': 3, '400': 4, '401': 2, '403': 6, '404': 6, '405': 4, '500': 4,}
-file_size_total = 0
-count = 0
+    statusCodes = {"200": 0,
+                   "301": 0,
+                   "400": 0,
+                   "401": 0,
+                   "403": 0,
+                   "404": 0,
+                   "405": 0,
+                   "500": 0
+                   }
+    fileSize = 0
+    n_lines = 0
 
-if __name__ == "__main__":
     try:
+        """ Read stdin line by line """
         for line in sys.stdin:
+            if n_lines != 0 and n_lines % 10 == 0:
+                """ After every 10 lines, print from the beginning """
+                print_results(statusCodes, fileSize)
+            n_lines += 1
+            data = line.split()
             try:
-                status_code = line.split()[-2]
-                if status_code in codes_count.keys():
-                    codes_count[status_code] += 1
-                # Grab file size
-                file_size = int(line.split()[-1])
-                file_size_total += file_size
-            except Exception:
+                """ Compute metrics """
+                statusCode = data[-2]
+                if statusCode in statusCodes:
+                    statusCodes[statusCode] += 1
+                fileSize += int(data[-1])
+            except:
                 pass
-            # print metrics if 10 lines have been read
-            count += 1
-            if count == 10:
-                print_metrics(file_size_total, codes_count)
-                count = 0
+        print_results(statusCodes, fileSize)
     except KeyboardInterrupt:
-        print_metrics(file_size_total, codes_count)
+        """ Keyboard interruption, print from the beginning """
+        print_results(statusCodes, fileSize)
         raise
-   print_metrics(file_size_total, codes_count)
